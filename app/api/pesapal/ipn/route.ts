5261d99caf35ca64f907fa16b490b3689e0ed680
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-25 23:08:30 +0300

    Navbar component in the layout

---
+++ app/api/pesapal/ipn/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse} from "next/server";
+
+export async function POST(req: Request) {
+	try {
+		const body = await req.json();
+		console.log("Pesapal IPN Recieved", body); // debugging
+
+		const { order_tracking_id, transaction _reference, status } = body;
+
+		//step: 1 Verify the transaction status with Pesapal
+		const verificationUrl = `${process.env.PESAPAL_API_URL}/Transactions/GetTransactionStatus?orderTrackingId=${order_tracking_id}`;
+		const response = await fetch(verificationUrl, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${process.env.PESAPAL_API_URL}`, //Get a fresh token first
+			}
+		})
+
+		const verificationData = await response.json();
+		console.log("Verification Data", verificationData);
+		if (verificationData.status === "Completed") {
+			console.log("Payment Successful", verificationData);
+			// Step 2: Update Database (Example)
+			//await db.updateTransaction(transaction_reference, {status: 'Paid"})
+		} else {
+			console.log("Payment Not Completed: ", verificationData);
+		}
+		return NextResponse.json({ message: "IPN Processed"})
+	} catch (error) {
+		console.error("IPN Error: ", error);
+		return NextResponse.json({ error: "Error Processing IPN"}, {status: 500})
+	}
+}
